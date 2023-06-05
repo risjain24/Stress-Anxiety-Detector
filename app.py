64fbe6498974c751c2bd35240041432cde901e61
risjain24 <rishabhjain240900@gmail.com>
2023-06-05 17:41:24 +0530

    Added progress Bar

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from keras.models import load_model
 import pandas as pd
 
 model = load_model('model.h5')
+max_words = 20000
+tokenizer = Tokenizer(num_words=max_words)
 
 def remove_urls(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(r'', text)
 
-def predict_stress_anxiety(stringInput):
+def preprocessing():
     datasetfinal = pd.read_csv("dataset_modedlled.csv")
     datasetfinal.rename(columns = {'Comments Text':'text'},inplace = True)
     for i in range(len(datasetfinal)):
@@ -25,9 +27,9 @@ def predict_stress_anxiety(stringInput):
     data = [sent.lower() for sent in data]
     data = [sent.replace('.', '') for sent in data]
     tweets = np.array(data)
-    max_words = 20000
-    tokenizer = Tokenizer(num_words=max_words)
     tokenizer.fit_on_texts(tweets)
+
+def predict_stress_anxiety(stringInput):
     stringInput.replace(r'[^\x00-\x7F]+','')
     stringInput = remove_urls(stringInput)
     data = [stringInput]
@@ -38,23 +40,39 @@ def predict_stress_anxiety(stringInput):
     sequences = tokenizer.texts_to_sequences(data)
     tweet = pad_sequences(sequences, maxlen=400)
     prediction = model.predict(tweet)
-    prediction = np.around(prediction, decimals=0)
-    if prediction == 1.0:
-        prediction = "Yes the statement shows stress and anxiety"
-    else:
-        prediction = "No the statement does not show Stress and Anxiety"
     return prediction
 
 def main():
+    preprocessing()
     st.title("Stress Anxiety Detector")
     input = st.text_input("Enter a Comment/Tweet","Type Here")
-    result = ''
+    result = 0.0
     if st.button("Predict"):
         result = predict_stress_anxiety(input)
-    st.success('Final Verdict: {}'.format(result))
-    if st.button("About"):
+        result = result[0][0].astype(float)
+        resApprox = np.around(result, decimals=0)
+        if resApprox == 1.0:
+            st.success('Yes the statement shows signs of stress and anxiety')
+            st.text("Confidence Score : " + str(result))
+            st.progress(result)
+        else:
+            st.markdown(
+                """
+                <div style="background-color: #f05b5b; padding: 10px; border-radius: 5px;">
+                <span style="color: white;">No the statement does not show signs of Stress and Anxiety</span>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+            st.write("")
+            st.text("Confidence Score : " + str(1-result))
+            st.progress((1-result))
+            
+    if st.button("Help"):
         st.text("This is a Stress and Anxiety Detector.")
         st.text(" Enter a User's Tweet or Facebook Comment \n Or any other post in order to detect presence of stress or Anxiety")
+    if st.button("About"):
+        st.text("Made with ♥ by Students of BVCOE, New Delhi")
 
 if __name__ == '__main__':
     main()
